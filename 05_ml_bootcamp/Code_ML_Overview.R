b51362d498186dded0b69d0576467438c5e012b4
NovaVolunteer <bwright.wright6@gmail.com>
2021-08-19 11:04:16 -0400

    Updating kNN Lab, starting work on the ML bootcamp

---
+++ 05_ml_bootcamp/Code_ML_Overview.R
@@ -0,0 +1,13 @@
+# Topics to Cover:  
+# Overview of Terms of the field - Model Types
+# Pipeline for ML
+# When to use ML and when no to
+# Independent Metric for Business Value 
+# Baseline
+# Parameters versus Hyperparameters 
+# Training, Tune, Evaluation
+# Cross Validation 
+# Bias versus Variance 
+# Boosting and Bagging 
+# Feature Engineering 
+# 
