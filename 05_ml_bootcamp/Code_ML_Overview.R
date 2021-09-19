1fd5d7facafbbd61beb40d2606ec6c91fcf88082
NovaVolunteer <bwright.wright6@gmail.com>
2021-09-19 15:26:36 -0400

    Updates to the labs

--- 05_ml_bootcamp/Code_ML_Overview.R
+++ 05_ml_bootcamp/Code_ML_Overview.R
@@ -1,13 +1,15 @@
 # Topics to Cover:  
 # Overview of Terms of the field - Model Types
-# Pipeline for ML
+# Probabilistic Interpretation 
+# Pipeline for ML - Computational 
 # When to use ML and when no to
 # Independent Metric for Business Value 
-# Baseline
-# Parameters versus Hyperparameters 
-# Training, Tune, Evaluation
-# Cross Validation 
-# Bias versus Variance 
-# Boosting and Bagging 
-# Feature Engineering 
-# 
+# Baseline - Computational
+# Parameters versus Hyperparameters - Computational
+# Thresholding - Computational
+# Scaling and/or Normalizing Data - Computational
+# Training, Tune, Evaluation - Computational
+# Cross Validation - Computational
+# Bias versus Variance - Maybe Computational 
+# Feature Engineering - Computational 
+ 
