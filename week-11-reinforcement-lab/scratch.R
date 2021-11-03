df4728453f1be5a67240441905c1c019b90d4265
pete <pete.alonzi@gmail.com>
2021-11-02 20:47:51 -0500

    Update scratch.R

--- week-11-reinforcement-lab/scratch.R
+++ week-11-reinforcement-lab/scratch.R
@@ -1,10 +1,10 @@
 library(tidyverse)
 
-# read in data and create dataframe
+# read in data and create dataframe (df1)
 df <- read_csv("data-summary.csv")
 df1 <- select(df,main_colors,opp_colors,on_play,num_turns,won)
 
-# feature engineering
+# feature engineering (cora,corc)
 df2 <- select(df,"deck_Adeline, Resplendent Cathar":"deck_Wrenn and Seven")
 mat = data.matrix(df2)
 vec1 <- vector()
@@ -15,8 +15,10 @@ for(i in 1:nrow(mat) ){
   z<-cor( mat[47,] , mat[i,])
   vec3 <- c(vec3,z)
 }
+
+# add new features to dataframe
 df1 <- df1 %>% mutate(cora = vec1)
 df1 <- df1 %>% mutate(corc = vec3)
 
-# make scatter plot
+# make scatter plot comparing new features
 ggplot(df1,aes(x=cora,y=corc))+geom_point()
